4075c8d04d4c2f00d3dad62d4fecc2cfa4e1eaa7
bojanbosnic <bojan.bosnicc99@gmail.com>
2023-01-02 00:46:04 +0100

    return effect

--- components/Hero.js
+++ components/Hero.js
@@ -14,47 +14,41 @@ const Hero = () => {
   return (
     <div className="container mx-auto">
       <div className="w-full flex mb-7 flex-wrap md:items-center md:flex-nowrap">
-        <div
-          className="w-full order-2 md:order-1 md:w-3/5"
-          data-aos="fade-up"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-        >
+        <div className="w-full order-2 md:order-1 md:w-3/5">
           <div className="designer-hero"></div>
           <h1
             className=" text-primary text-center md:text-left uppercase"
-            // data-aos="fade-up"
-            // data-aos-delay="51"
-            // data-aos-duration="1000"
-            // data-aos-easing="ease-in-out"
+            data-aos="fade-up"
+            data-aos-delay="51"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
           >
             Dejan Skondric
           </h1>
           <span
             className="block text-xl text-center font-normal leading-9  md:text-left md:text-5xl md:leading-[78px]"
-            // data-aos="fade-up"
-            // data-aos-delay="52"
-            // data-aos-duration="1000"
-            // data-aos-easing="ease-in-out"
+            data-aos="fade-up"
+            data-aos-delay="52"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
           >
             Hi there, I’m a Freelance
           </span>
           <span
             className="block text-4xl leading-9 text-center font-extrabold md:text-left md:text-7xl md:leading-[78px]"
-            // data-aos="fade-up"
-            // data-aos-delay="53"
-            // data-aos-duration="1000"
-            // data-aos-easing="ease-in-out"
+            data-aos="fade-up"
+            data-aos-delay="53"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
           >
             Product Designer
           </span>
           <h2
             className="my-3 text-center max-width  md:text-left"
-            // data-aos="fade-up"
-            // data-aos-delay="50"
-            // data-aos-duration="1000"
-            // data-aos-easing="ease-in-out"
+            data-aos="fade-up"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
           >
             I help companies to design digital products and turn ideas into a
             functioanl and delightfull experience. I’m focusing on working on
@@ -63,10 +57,10 @@ const Hero = () => {
           </h2>
           <h3
             className="my-5 text-center md:text-left"
-            // data-aos="fade-up"
-            // data-aos-delay="50"
-            // data-aos-duration="1000"
-            // data-aos-easing="ease-in-out"
+            data-aos="fade-up"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
           >
             <a className="cursor-pointer" href="#projects">
               View Projects
